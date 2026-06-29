e06b450d8ea362de71a065a738428f9f0526f136
Shivam Agrawal <agrawal.shivam127@gmail.com>
2026-06-29 09:07:06 +0530

    Add and delete file folders functionality

--- machine-coding/folder-flow/src/App.js
+++ machine-coding/folder-flow/src/App.js
@@ -4,6 +4,7 @@ import { explorerData } from './constants';
 
 function App() {
   const [expandedFolders,setExpandedFolders] = useState({});
+  const [updatedExplorerData,setExplorerData] = useState(explorerData);
 
   function toggleState(event,id){
     event.stopPropagation();
@@ -12,16 +13,62 @@ function App() {
     })
   }
 
+  function updateTree(nodes,targetId,newFile){
+    return nodes?.map((node)=>{
+      if(node.id === targetId){
+        return {
+          ...node,children:[...node.children,newFile]
+        }
+      } 
+      else if(node.children){
+        console.log(node.children);
+        return {
+          ...node,children:updateTree(node.children,targetId,newFile)
+        }
+      }
+      else {
+        return node;
+      }
+    })
+  }
+
+  function addNew(id,type) {
+    const newFile = type === 'file' ? {id:Math.floor(Math.random()*1000),name:'New file.js',type:'file'}: {id:Math.floor(Math.random()*1000),name:'New folder',type:'folder',children:[]} ;
+    setExplorerData((prev)=>{
+        return updateTree(prev,id,newFile);
+    })
+    setExpandedFolders((prev)=>{
+      return {...prev,[id]:true}
+    })
+  }
+
+  function filterData(nodes,currNode){
+    return nodes.filter(node=>node.id!==currNode.id)
+  .map((node)=> {
+     if(node.children){
+      return {...node,children:filterData(node.children,currNode)}
+     }
+     return node;
+  })}
+
+  function deleteFolder(node){
+    setExplorerData((prev)=> filterData(prev,node));
+    alert('Deleted successfully');
+  }
+
   function renderNodes(data) {
     return data.map(node => {
       if (node.children) {
         return <li key={node.id} ><span onClick={(e) => toggleState(e, node.id)}>
           {node.name}
         </span>
+        <button onClick={()=> addNew(node.id,'file')}> Create File</button>
+         <button onClick={()=> addNew(node.id,'folder')}> Create Folder</button>
+         <button onClick={() => deleteFolder(node)}>Delete Folder</button>
           {expandedFolders[node.id] && <ul>{renderNodes(node.children)}</ul>}
         </li>
       } else {
-        return <li key={Math.round(node.id).toFixed(0)}>{node.name}</li>
+        return <li key={Math.round(node.id).toFixed(0)}>{node.name} <button onClick={() => deleteFolder(node)}>Delete File</button></li>
       }
     }
     )
@@ -30,7 +77,7 @@ function App() {
     <>
       <h1> Welcome to the folder-flow structure</h1>
       <ul>
-        {renderNodes(explorerData)}
+        {renderNodes(updatedExplorerData)}
       </ul>
     </>
   );
